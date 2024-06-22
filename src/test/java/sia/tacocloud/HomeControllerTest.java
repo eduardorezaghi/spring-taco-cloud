f8e08a2151ebaa7659fa4b6cdb80142c5b2ba5b1
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2024-06-22 08:13:57 -0300

    Add HomeController test

--- src/test/java/sia/tacocloud/HomeControllerTest.java
+++ src/test/java/sia/tacocloud/HomeControllerTest.java
@@ -1,9 +1,23 @@
 package sia.tacocloud;
 
-import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(HomeController.class)
 public class HomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
     public void testHome() {
         // Arrange
@@ -15,4 +29,13 @@ public class HomeControllerTest {
         // Assert
         assertEquals("home", viewName);
     }
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("home"))
+            .andExpect(content().string(containsString("Welcome to...")));
+    }
+
 }
