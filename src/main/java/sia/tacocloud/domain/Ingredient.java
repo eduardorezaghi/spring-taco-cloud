c6003a0c92ef1a50cd39579068eef48dd392248d
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2024-06-23 22:43:00 -0300

    Add lombok and Domain classes (models)

---
+++ src/main/java/sia/tacocloud/domain/Ingredient.java
@@ -0,0 +1,13 @@
+package sia.tacocloud.domain;
+
+import lombok.Data;
+
+@Data
+public class Ingredient {
+    private final String id;
+    private final String name;
+    private final Type type;
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
